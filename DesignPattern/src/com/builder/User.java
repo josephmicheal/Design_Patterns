b69ab022223367c031e7da3232d43edc30a3a8b6
josephmicheal <sun.leo04@gmail.com>
2022-01-02 09:43:06 +0530

    updated

--- DesignPattern/src/com/builder/User.java
+++ DesignPattern/src/com/builder/User.java
@@ -77,13 +77,13 @@ public class User
 	
 	public static void main(String[] args) 
 	{
-		User user1 = new User.UserBuilder("Lokesh", "Gupta")
+		User employee = new User.UserBuilder("Lokesh", "Gupta")
 		.age(30)
 		.phone("1234567")
 		.address(new UserAddress("Fake","city", "1234"))
 		.build();
 
-		System.out.println(user1);
+		System.out.println(employee);
 
 		User user2 = new User.UserBuilder("Jack", "Reacher")
 		.age(40)
@@ -93,13 +93,13 @@ public class User
 
 		System.out.println(user2);
 
-		User user3 = new User.UserBuilder("Super", "Man")
+		User deliveryBoy = new User.UserBuilder("Super", "Man")
 		//No age
 		//No phone
 		//no address
 		.build();
 
-		System.out.println(user3);
+		System.out.println(deliveryBoy);
 	}  
 }
 class UserAddress{
