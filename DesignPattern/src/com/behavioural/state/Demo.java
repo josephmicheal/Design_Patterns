d7d0b942279f4cd05dc13cb3efe866ae73fb1dd3
josephmicheal <sun.leo04@gmail.com>
2022-02-16 21:27:09 +0530

    updated

---
+++ DesignPattern/src/com/behavioural/state/Demo.java
@@ -0,0 +1,11 @@
+package com.behavioural.state;
+/**
+ * Demo class. Everything comes together here.
+ */
+public class Demo {
+    public static void main(String[] args) {
+        Player player = new Player();
+        UI ui = new UI(player);
+        ui.init();
+    }
+}
