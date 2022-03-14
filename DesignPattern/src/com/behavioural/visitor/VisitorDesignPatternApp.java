07b236342e8e7b3b4007151bc64c2a4dfc7642e6
josephmicheal <sun.leo04@gmail.com>
2022-03-14 07:40:35 +0530

    updated

--- DesignPattern/src/com/behavioural/visitor/VisitorDesignPatternApp.java
+++ DesignPattern/src/com/behavioural/visitor/VisitorDesignPatternApp.java
@@ -1,71 +1,65 @@
 package com.behavioural.visitor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class VisitorDesignPatternApp {
 	
-	
 	public static void main(String args[]) {
-		List<Shape> shapes = new ArrayList<>();
-		shapes.add(new VisitorDesignPatternApp().new Circle());
-		shapes.add(new VisitorDesignPatternApp().new Rectangle());
+		Shape circle = new Circle();
+		Shape rectangle = new Rectangle();
 		
-		Visitor visitor = new VisitorDesignPatternApp().new ExportVisitor();
-		shapes.forEach(shape -> {
-			shape.accept(visitor);
-		});
+		Visitor visitor = new XMLExportVisitor();
+		circle.accept(visitor);
+		rectangle.accept(visitor);
 		
 	}
 
-	interface Shape {
-		void accept(Visitor v);
+}
+
+interface Shape {
+	void accept(Visitor v);
+}
+
+class Circle implements Shape {
+
+	int radius = 5;
+
+	@Override
+	public void accept(Visitor v) {
+
+		v.visitCircle(this);
 	}
 
-	class Circle implements Shape {
+}
 
-		private int radius = 5;
+class Rectangle implements Shape {
 
-		@Override
-		public void accept(Visitor v) {
+	int width = 5;
+	int breath = 5;
 
-			v.visitCircle(this);
-		}
+	@Override
+	public void accept(Visitor v) {
+
+		v.visitRectangle(this);
+	}
+
+}
+
+interface Visitor {
+	void visitCircle(Circle c);
+
+	void visitRectangle(Rectangle c);
+}
+
+class XMLExportVisitor implements Visitor {
+
+	@Override
+	public void visitCircle(Circle c) {
+		System.out.println("<XML> Circle with raidus " + c.radius);
 
 	}
 
-	class Rectangle implements Shape {
-
-		private int width = 5;
-		private int breath = 5;
-
-		@Override
-		public void accept(Visitor v) {
-
-			v.visitRectangle(this);
-		}
-
-	}
-
-	interface Visitor {
-		void visitCircle(Circle c);
-
-		void visitRectangle(Rectangle c);
-	}
-
-	class ExportVisitor implements Visitor {
-
-		@Override
-		public void visitCircle(Circle c) {
-			System.out.println("Circle with raidus " + c.radius);
-
-		}
-
-		@Override
-		public void visitRectangle(Rectangle c) {
-			System.out.println("Rectangle with width " + c.width + " breath " + c.breath);
-
-		}
+	@Override
+	public void visitRectangle(Rectangle c) {
+		System.out.println("<XML> Rectangle with width " + c.width + " breath " + c.breath);
 
 	}
 
