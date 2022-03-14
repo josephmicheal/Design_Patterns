e449949de425a18af178817f85f471e2cd899924
josephmicheal <sun.leo04@gmail.com>
2022-03-14 08:31:52 +0530

    updated

--- DesignPattern/src/com/creational/factory/AbstractFactoryApp.java
+++ DesignPattern/src/com/creational/factory/AbstractFactoryApp.java
@@ -1,109 +1,111 @@
 package com.creational.factory;
 
 public class AbstractFactoryApp {
-	
+
 	private static Application configureApplication() {
-        Application app;
-        GUIFactory factory;
-        String osName = System.getProperty("os.name").toLowerCase();
-        if (osName.contains("mac")) {
-            factory = new AbstractFactoryApp().new MacOSFactory();
-            app = new AbstractFactoryApp().new Application(factory);
-        } else {
-            factory = new AbstractFactoryApp().new WindowsFactory();
-            app = new AbstractFactoryApp().new Application(factory);
-        }
-        return app;
-    }
-
-    public static void main(String[] args) {
-        Application app = configureApplication();
-        app.paint();
-    }
-
-	public class Application {
-	    private Button button;
-	    private Checkbox checkbox;
-
-	    public Application(GUIFactory factory) {
-	        button = factory.createButton();
-	        checkbox = factory.createCheckbox();
-	    }
-
-	    public void paint() {
-	        button.paint();
-	        checkbox.paint();
-	    }
+		Application app;
+		GUIFactory factory;
+		String osName = System.getProperty("os.name").toLowerCase();
+		if (osName.contains("mac")) {
+			factory = new MacOSFactory();
+			app = new Application(factory);
+		} else {
+			factory = new WindowsFactory();
+			app = new Application(factory);
+		}
+		return app;
 	}
-	
-	
-	public interface Button {
-	    void paint();
-	}
-	public interface Checkbox {
-	    void paint();
-	}
-	
-	public class MacOSButton implements Button {
 
-	    @Override
-	    public void paint() {
-	        System.out.println("You have created MacOSButton.");
-	    }
+	public static void main(String[] args) {
+		Application app = configureApplication();
+		app.paint();
 	}
-	
-	public class WindowsButton implements Button {
 
-	    @Override
-	    public void paint() {
-	        System.out.println("You have created WindowsButton.");
-	    }
+}
+
+class Application {
+	private Button button;
+	private Checkbox checkbox;
+
+	public Application(GUIFactory factory) {
+		button = factory.createButton();
+		checkbox = factory.createCheckbox();
 	}
-	
-	public class MacOSCheckbox implements Checkbox {
 
-	    @Override
-	    public void paint() {
-	        System.out.println("You have created MacOSCheckbox.");
-	    }
+	public void paint() {
+		button.paint();
+		checkbox.paint();
 	}
-	public class WindowsCheckbox implements Checkbox {
+}
 
-	    @Override
-	    public void paint() {
-	        System.out.println("You have created WindowsCheckbox.");
-	    }
+interface Button {
+	void paint();
+}
+
+interface Checkbox {
+	void paint();
+}
+
+class MacOSButton implements Button {
+
+	@Override
+	public void paint() {
+		System.out.println("You have created MacOSButton.");
 	}
-	
-	public interface GUIFactory {
-	    Button createButton();
-	    Checkbox createCheckbox();
+}
+
+class WindowsButton implements Button {
+
+	@Override
+	public void paint() {
+		System.out.println("You have created WindowsButton.");
 	}
-	
-	public class MacOSFactory implements GUIFactory {
+}
 
-	    @Override
-	    public Button createButton() {
-	        return new MacOSButton();
-	    }
+class MacOSCheckbox implements Checkbox {
 
-	    @Override
-	    public Checkbox createCheckbox() {
-	        return new MacOSCheckbox();
-	    }
+	@Override
+	public void paint() {
+		System.out.println("You have created MacOSCheckbox.");
 	}
-	
-	public class WindowsFactory implements GUIFactory {
+}
 
-	    @Override
-	    public Button createButton() {
-	        return new WindowsButton();
-	    }
+class WindowsCheckbox implements Checkbox {
 
-	    @Override
-	    public Checkbox createCheckbox() {
-	        return new WindowsCheckbox();
-	    }
+	@Override
+	public void paint() {
+		System.out.println("You have created WindowsCheckbox.");
 	}
-	
+}
+
+interface GUIFactory {
+	Button createButton();
+
+	Checkbox createCheckbox();
+}
+
+class MacOSFactory implements GUIFactory {
+
+	@Override
+	public Button createButton() {
+		return new MacOSButton();
+	}
+
+	@Override
+	public Checkbox createCheckbox() {
+		return new MacOSCheckbox();
+	}
+}
+
+class WindowsFactory implements GUIFactory {
+
+	@Override
+	public Button createButton() {
+		return new WindowsButton();
+	}
+
+	@Override
+	public Checkbox createCheckbox() {
+		return new WindowsCheckbox();
+	}
 }
